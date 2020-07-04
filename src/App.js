aa7aa3edc9a4faf728370874b8e04b5a9ccd1f33
huynheddie <huynheddie@yahoo.com>
2020-07-04 14:52:43 -0700

    Finished setting up eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>
+        Counter:
+        {counter}
+      </h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
