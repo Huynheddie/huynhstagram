4f7f51f192c84afe96311c1866abdabf46e72a6b
huynheddie <huynheddie@yahoo.com>
2020-08-10 11:29:18 -0700

    Fix modal closing on user lits

--- src/components/Posts/UserThumbnail.js
+++ src/components/Posts/UserThumbnail.js
@@ -4,10 +4,10 @@ import Image from 'cloudinary-react/lib/components/Image';
 import Transformation from 'cloudinary-react/lib/components/Transformation';
 import { Link } from 'react-router-dom';
 
-const UserThumbnail = ({ userId, profileImage, width, height, color }) => (
+const UserThumbnail = ({ userId, profileImage, width, height, color, handleCloseModal }) => (
   <>
     <CloudinaryContext cloudName='huynhstagram'>
-      <Link to={`/user/${userId}`}>
+      <Link to={`/user/${userId}`} onClick={handleCloseModal || null}>
         <Image publicId={profileImage} className='comment-profile-image'>
           <Transformation
             width={width || '30'}
